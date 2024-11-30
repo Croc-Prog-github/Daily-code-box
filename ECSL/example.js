66e95d02b2d20e5c413604e9a7012ec2f1f4111b
Croc-Prog-github <119326147+Croc-Prog-github@users.noreply.github.com>
2024-11-30 14:22:15 +0100

    2 file edited

--- ECSL/example.js
+++ ECSL/example.js
@@ -4,5 +4,4 @@ const resistanceResult = ECSL.resistence("Resistor1", 1000, 5, 5);
 const capacitorResult = ECSL.capacitor("Capacitor1", 0.000001, 5, 1); // 1 microfarad, 5V, 1 secondo
 const inductorResult = ECSL.inductor("Inductor1", 0.01, 0, 1, 5); // 10mH, 0A iniziale, 1 secondo, 5V
 
-
 console.log(resistanceResult, capacitorResult);
